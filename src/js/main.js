aa7bcfcefb865e1daf7f403f01b8a0faedf83e3f
Badra Fellah <fellah.badra.2021.2022@gmail.com>
2023-08-10 15:27:11 +0100

    devenirexpert2

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,71 @@
 "use strict";
-import"slick-carousel";
+import "jquery";
+import "jquery-ui/dist/jquery-ui";
+import "slick-carousel";
 
-$('.slider-image').slick();
+
+
+
+// Class definition
+var App = (function () {
+  
+  const initSliders = () => {
+    $(".slider-img").slick({
+      slidesToShow: 5,
+      slidesToScroll: 2,
+      dots: false,
+      autoplay: false,
+      autoplaySpeed: 3000,
+      arrows: false,
+
+    });
+
+    //j'ajoute la classe ici et je la configure comme ci-dessus
+      
+    $(".slider").slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: false,
+      autoplay: false,
+      autoplaySpeed: 3000,
+      arrows: false,
+     
+    });
+
+
+    $(".slider-reviews").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: false,
+      autoplay: false,
+      autoplaySpeed: 3000,
+      arrows: false,
+     
+    });
+
+
+    }
+   
+
+  // Public methods
+  return {
+    init: function () {
+      initSliders();
+    },
+  };
+})();
+
+// Class Initialization
+jQuery(document).ready(function () {
+  App.init();
+});
+
+
+//function
+// function previous() {
+
+// }
+
+// function next() {
+//   document.querySelector(".slider").scrollLeft += 60;
+// }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,26 @@
-let mix = require('webpack-mix');
+const mix = require('webpack-mix');
+const webpack = require("webpack");
+
+// const mix = require('laravel-mix');
+
+mix.webpackConfig({
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      }),
+    ],
+  });
+
 
 mix.js('src/js/main.js', 'dist/js/main.min.js')
     .sass('src/scss/style.scss', 'dist/css/style.min.css')
     .options({processCssUrls: false});
 
+    mix.copyDirectory(
+        "node_modules/slick-carousel/slick/fonts",
+        "assets/dist/css/fonts"
+      );
 
-
+     
